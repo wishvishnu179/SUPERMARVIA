0bdce2a2b8e9209f873596f75cd41525892e79db
Vishnu MB <137043572+wishvishnu179@users.noreply.github.com>
2024-12-12 11:46:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return """
-<center> 
-    <img src="https://cdn.jsdelivr.net/gh/wukong-i/static@main/channel%20img.jpg" style="border-radius: 12px;"/> 
-</center> 
-<style>
-    body { 
-        background: antiquewhite;
-    }
-</style>"""
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/")
+def hello_world():
+    return "Jisshu bots"
