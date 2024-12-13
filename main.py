4654237f9a1f6a459bc77a6bc79a0c1303ba46a6
Vishnu MB <137043572+wishvishnu179@users.noreply.github.com>
2024-12-13 17:36:58 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Koyeb! This is a web server."
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
