32b5746eb95b2f24ee3546d371241e6c45b6b9de
Vishnu MB <137043572+wishvishnu179@users.noreply.github.com>
2024-12-13 18:02:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-   import os
-   from flask import Flask
+import os
+from flask import Flask
 
-   app = Flask(__name__)
+app = Flask(__name__)
 
-   @app.route("/")
-   def hello():
-       return "Hello from Koyeb! Simple test."
+@app.route("/")
+def hello():
+    try:
+        return "Hello from Koyeb!"
+    except Exception as e:
+        return f"An error occurred: {e}"
 
-   if __name__ == "__main__":
-       app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
-
-   
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
