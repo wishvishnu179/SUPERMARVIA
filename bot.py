c4fe6c4ac8973837b19818cf3bd1cf35b6892219
Vishnu MB <137043572+wishvishnu179@users.noreply.github.com>
2024-12-13 13:29:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,12 @@
+import logging
+import logging.config
+
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
+
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
