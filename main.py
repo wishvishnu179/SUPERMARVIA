a50066ad67f79963ac89280980b4f5a592b54b97
Vishnu MB <137043572+wishvishnu179@users.noreply.github.com>
2024-12-13 18:13:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ def hello():
         return f"An error occurred: {e}"
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
