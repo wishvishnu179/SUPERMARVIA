23bd538930845ea1fd165e01e360467971ad5be3
Vishnu MB <137043572+wishvishnu179@users.noreply.github.com>
2024-12-13 14:20:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,6 @@ from pyrogram import utils as pyroutils
 pyroutils.MIN_CHAT_ID = -999999999999
 pyroutils.MIN_CHANNEL_ID = -100999999999999
 
-PORT = environ.get("PORT", "8080")
 
 class Bot(Client):
 
