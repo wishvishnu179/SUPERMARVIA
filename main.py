06f125c64cca359f863c37d45d19c445864e7aa1
Vishnu MB <137043572+wishvishnu179@users.noreply.github.com>
2024-12-13 17:50:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import os
-from flask import Flask
+   import os
+   from flask import Flask
 
-app = Flask(__name__)
+   app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Koyeb! This is a web server."
+   @app.route("/")
+   def hello():
+       return "Hello from Koyeb! Simple test."
 
-if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+   if __name__ == "__main__":
+       app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
+   
