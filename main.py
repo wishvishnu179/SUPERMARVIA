32af74256d69c6fdf83a1f5bfb22009a1d95b60b
Vishnu MB <137043572+wishvishnu179@users.noreply.github.com>
2024-12-13 21:56:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
