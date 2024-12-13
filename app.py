41ecb45e6b9891d9f1baed3f2b7887a811c6f5c5
Vishnu MB <137043572+wishvishnu179@users.noreply.github.com>
2024-12-13 14:42:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello_world():
-    return "SUPERMARVIA"
+def home():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    # Use the port from the environment or default to 8000
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
