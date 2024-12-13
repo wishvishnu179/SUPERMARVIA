8a662fa5c70feb5a5b5c4a7637eb900f82c84a94
Vishnu MB <137043572+wishvishnu179@users.noreply.github.com>
2024-12-13 14:54:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,3 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, World!"
-
-if __name__ == "__main__":
-    # Use the port from the environment or default to 8000
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
