29134b677397b3e5e327a829978d187d20de4be8
Vishnu MB <137043572+wishvishnu179@users.noreply.github.com>
2024-12-14 10:32:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,21 @@ logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
-# ... (Your imports remain unchanged) ...
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, LOG_STR
+from utils import temp
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
+from Script import script 
+from aiohttp import web
+from datetime import date, datetime 
+import pytz
+from pyrogram import utils as pyroutils
+pyroutils.MIN_CHAT_ID = -999999999999
+pyroutils.MIN_CHANNEL_ID = -100999999999999
 
 class Bot(Client):
     
